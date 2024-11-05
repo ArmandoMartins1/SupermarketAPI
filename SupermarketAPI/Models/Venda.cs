e040fbcdd110927e2f8bd8e634ea58e98967454c
DGuabiraba <daviguabiraba00@gmail.com>
2024-11-05 16:38:43 -0300

    refactor: atualizar múltiplos arquivos do projeto e remover migrações obsoletas
    - refactor: atualizados controladores, DTOs, modelos, serviços e arquivos de configuração
    - fix: resolver mudanças no ProdutoController e ProdutoService para alinhar com a nova estrutura de DTOs
    - feat: adicionar middleware para tratamento de erros e logging de requisições
    - build: remover arquivos de migração antigos para redefinir o esquema do banco de dados
    - docs: atualizar README com instruções de configuração atuais e dados de exemplo
    - chore: atualizar configurações do projeto e arquivos appsettings para o ambiente de desenvolvimento
    - test: integrar novos arquivos de teste para testes de unidade e integração

--- SupermarketAPI/Models/Venda.cs
+++ SupermarketAPI/Models/Venda.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,8 @@ namespace SupermarketAPI.Models
         public int Id { get; set; }
         public int ProdutoId { get; set; }
         public int Quantidade { get; set; }
+        
+        [Column(TypeName = "decimal(10, 2)")]
         public decimal ValorTotal { get; set; }
         public DateTime DataVenda { get; set; }
 
--- SupermarketAPI/obj/Debug/net8.0/SupermarketAPI.AssemblyInfo.cs
+++ SupermarketAPI/obj/Debug/net8.0/SupermarketAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupermarketAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10c978ce6dd6f3dbcaa0fc1709ed17bd9525ea3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+683bf5f384c56b17a3921875e90b5511550fa9ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupermarketAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupermarketAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
